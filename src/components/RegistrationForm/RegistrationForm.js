f7423c229bf922b11bfdeebebb8daac61a2e973c
Jordan <jordanknox@Jordans-MacBook-Pro-3.local>
2020-05-18 22:14:11 -0700

    latest

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -83,7 +83,7 @@ class RegistrationForm extends Component {
             Sign up
           </Button>
           {' '}
-          <Link to='/login'>Already have an account?</Link>
+          <Link className="accountLink"to='/login'>Already have an account?</Link>
         </footer>
       </form>
     )
